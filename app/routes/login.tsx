b0a638ad303080d3f27058ae60b9287ad6f6b09c
amabir <amabirbd@gmail.com>
2022-04-15 23:34:48 +0600

    session login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -11,7 +11,8 @@ import type {
   
   import { db } from "~/utils/db.server";
   import stylesUrl from "~/styles/login.css";
-  
+  import { createUserSession, login } from "~/utils/session.server";
+
   export const links: LinksFunction = () => {
     return [{ rel: "stylesheet", href: stylesUrl }];
   };
@@ -84,14 +85,23 @@ import type {
   
     switch (loginType) {
       case "login": {
-        // login to get the user
-        // if there's no user, return the fields and a formError
-        // if there is a user, create their session and redirect to /jokes
-        return badRequest({
-          fields,
-          formError: "Not implemented",
-        });
+        const user = await login({ username, password });
+        console.log({ user });
+        if (!user) {
+          return badRequest({
+            fields,
+            formError: `Username/Password combination is incorrect`,
+          });
+          
+        }
+        // return badRequest({
+        //   fields,
+        //   formError: "Not implemented",
+        // });
+        return createUserSession(user.id, redirectTo);
+
       }
+
       case "register": {
         const userExists = await db.user.findFirst({
           where: { username },
@@ -109,6 +119,7 @@ import type {
           formError: "Not implemented",
         });
       }
+      
       default: {
         return badRequest({
           fields,
